fade832ac78b7d9bb889ee9b86c883e512a647bc
NunesArthu <165511550+NunesArthu@users.noreply.github.com>
2024-06-09 21:40:27 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,13 @@
+
+document.getElementById("t1").addEventListener("click", function() {
+document.getElementById("NP-evento").scrollIntoView({behavior: "smooth"});
+});
+
+document.getElementById("e2-e").addEventListener("click", function() {
+document.getElementById("NP2-evento").scrollIntoView({behavior: "smooth"});
+});
+
+document.getElementById("e1-e").addEventListener("click", function() {
+document.getElementById("NP-evento").scrollIntoView({behavior: "smooth"});
+});
+
